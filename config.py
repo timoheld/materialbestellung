95cbac1273724411828ef3eb5306f46596815de4
timoheld <60256244+timoheld@users.noreply.github.com>
2023-03-25 16:21:10 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or '123456'
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:Master01@materialbestellung-mysqldb-1:3306/materialbestellung" 
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
