fc77e0d39214fc83798aa258f755c9f060c07204
timoheld <60256244+timoheld@users.noreply.github.com>
2023-03-26 11:40:55 +0200

    Update __init__.py
    compare_type=True added

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ from flask_httpauth import HTTPBasicAuth
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+migrate = Migrate(app, db, compare_type=True)
 login = LoginManager(app)
 login.login_view = 'login'
 
